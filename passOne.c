4f6a9bee57860f1e4c53a17eca834a6a9e031076
Ashfaaq Mohammed <iim.ashfaaq@gmail.com>
2020-04-02 19:52:39 +0530

    Initial code added

---
+++ passOne.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+    authors:    iimashfaaq
+                prachidhingra09
+*/
+
+void main() {
+    FILE *inp, *symTab, *opTab;
+    clrscr();
+
+    getch();
+}
